c968b9a67bbebf546cfb1e957dce128a22b80129
k7aled <khd.rashidi@gmail.com>
2025-01-02 00:15:12 +0330

    add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+// import components
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
