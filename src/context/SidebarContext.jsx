98adbd4c99e922e81e5b928fde2d38aee9108f01
k7aled <khd.rashidi@gmail.com>
2025-01-02 16:12:56 +0330

    create UseSidebar custom hook

--- src/context/SidebarContext.jsx
+++ src/context/SidebarContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useContext } from "react";
 const sidebarContext = createContext();
 const SidebarProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,4 +12,10 @@ const SidebarProvider = ({ children }) => {
   );
 };
 
+const UseSidebar = () => {
+  const data = useContext(sidebarContext);
+  return data;
+};
+
 export default SidebarProvider;
+export { UseSidebar };
