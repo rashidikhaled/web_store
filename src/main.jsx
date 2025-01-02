850c7d0bc060bddc1a601380f866129944df3e97
k7aled <khd.rashidi@gmail.com>
2025-01-02 16:03:03 +0330

    wrap SidebarProvider to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ProductsProvider from "./context/ProductsContext.jsx";
+import SidebarProvider from "./context/SidebarContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ProductsProvider>
-      <App />
-    </ProductsProvider>
+    <SidebarProvider>
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
+    </SidebarProvider>
   </StrictMode>
 );
