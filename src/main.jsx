cae62aa0fa6f1bf4de2f2e7a94db427d53875e2a
k7aled <khd.rashidi@gmail.com>
2025-01-04 01:08:19 +0330

    wrap CartProvider to main

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import ProductsProvider from "./context/ProductsContext.jsx";
 import SidebarProvider from "./context/SidebarContext.jsx";
+import CartProvider from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <SidebarProvider>
-      <ProductsProvider>
-        <App />
-      </ProductsProvider>
+      <CartProvider>
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+      </CartProvider>
     </SidebarProvider>
   </StrictMode>
 );
