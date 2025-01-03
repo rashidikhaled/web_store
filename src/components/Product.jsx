6b62ed81ffe4a42a917f6ca7d29df1e94d21edce
k7aled <khd.rashidi@gmail.com>
2025-01-04 01:09:00 +0330

    useCart hook in product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Link, Links } from "react-router-dom";
 import { BsPlus, BsEyeFill } from "react-icons/bs";
 import { shortenText } from "../helper/helper";
+import { UseCart } from "../context/CartContext";
 
 const Product = ({ product }) => {
   // destructure product
   const { id, image, category, price, title } = product;
+  const { addToCart } = UseCart();
+
   return (
     <div>
       <div className="border border-[#e4e4e4] h-[300px] w-[300px]  mb-4 relative overflow-hidden group transition">
@@ -18,13 +21,16 @@ const Product = ({ product }) => {
             />
           </div>
 
-          <div className="absolute top-0 right-0 bg-red-500 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100">
+          <div className="absolute top-0 right-0 p-2 mr-3 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100">
             <button>
-              <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
-                <BsPlus className="text-3xl" />
+              <div className="flex justify-center items-center text-white w-12 h-12 bg-red-400">
+                <BsPlus
+                  onClick={() => addToCart(product, id)}
+                  className="text-3xl"
+                />
               </div>
             </button>
-            <Link className="w-12 h-12 bg-white flex justify-center items-center ">
+            <Link className="w-12 h-12 border border-pink-100 bg-white flex justify-center items-center ">
               <BsEyeFill />
             </Link>
           </div>
