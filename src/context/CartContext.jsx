8804c58753e50c899d9d0ba98739e63fc39d5de6
k7aled <khd.rashidi@gmail.com>
2025-01-04 01:07:36 +0330

    create CartContextProvider

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext, createContext, useState } from "react";
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    const cartItem = cart.find((item) => item.id === id);
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: newItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+  };
+
+  return (
+    <CartContext.Provider value={{ addToCart, cart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
-export default CartContext;
+const UseCart = () => {
+  const data = useContext(CartContext);
+  return data;
+};
+
+export default CartProvider;
+export { UseCart };
