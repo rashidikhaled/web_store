064a2baafc99fd32e2bdd38cc96c6f444ee59627
k7aled <khd.rashidi@gmail.com>
2025-01-04 01:09:59 +0330

    UseSidebar hook in Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { UseSidebar } from "../context/SidebarContext";
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { isOpen, setIsOpen } = UseSidebar();
+  return (
+    <div className="bg-pink-300 w-full">
+      <div>header</div>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="cursor-pointer flex relative">
+        <BsBag className="text-2xl" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
